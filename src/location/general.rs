a782848ebdb8ffc089161f3ca875482b2458426e
brunoczim <brunoczim@gmail.com>
2023-09-11 15:27:54 -0300

    better idea stub

--- src/location/general.rs
+++
@@ -1,8 +0,0 @@
-use super::{buf::DefaultOwnedStr, fragment::Fragment, internal::Internal};
-use url::Url;
-
-#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub enum Location<FS = Vec<Fragment>, I = DefaultOwnedStr, E = Url> {
-    Internal(Internal<FS, I>),
-    External(E),
-}
--- src/location/parse.rs
+++
@@ -1,7 +0,0 @@
-use std::error::Error;
-
-pub trait Parse<'input>: Sized {
-    type Error: Error;
-
-    fn parse(input: &'input str) -> Result<Self, Self::Error>;
-}
